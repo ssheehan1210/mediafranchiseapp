d7f814ef1eed67765026a2bd761051b0f36bef75
Sam Sheehan <ssheehan1210@yahoo.com>
2017-10-25 13:42:32 -0500

    Updated my version of the controllers

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,13 +1,57 @@
 const express = require('express');
 const router = express.Router();
-const movies = require('../models/movies');
+const Movies = require('../models/movies');
 
 router.get('/', (req, res) => {
-	res.render('movies/index', {movies: movies});
-})
+	Movies.find((err, movies) => {
+		res.render('movies/index', {movies: movies});
+	})
+});
+
+router.post('/', (req, res) => {
+	Movies.create(req.body, (err, newMovie) => {
+		if (err) {
+			res.send('error');
+		} else {
+			console.log(newMovie);
+			res.redirect('movies/');
+		}
+	})
+});
+
+router.get('/new', (req, res) => {
+	res.render('movies/new', {});
+});
 
 router.get('/:index', (req, res) => {
-	res.render('movies/show', {movie: movies[req.params.index]});
+	req.params.index = parseInt(req.params.index);
+	Movies.find((err, movies) => {
+		let ticker = 0;
+		for (let i = 0; i < movies.length; i++){
+			if (movies[i].id === req.params.index){
+				res.render('movies/show', {movie: movies[i]});
+			} else {
+				ticker++;
+				if (ticker === movies.length){
+					res.redirect('/movies')
+				}
+			}
+		}
+	})
+});
+
+router.get('/:index/addchar', (req, res) => {
+	Movies.findOne({id: req.params.index}, (err, foundMovie) =>{
+		res.render('movies/addcharacters', {movie: foundMovie});
+	})
+})
+
+router.put('/:index/addchar', (req, res) => {
+	Movies.findOne({id: req.params.index}, (err, foundMovie) => {
+		foundMovie.characters.push(req.body.name);
+		foundMovie.save();
+		res.redirect('/movies/:index');
+	})
 })
 
 module.exports = router;
